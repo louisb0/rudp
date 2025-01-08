cd8bfb2fe808444e83f4fbd638b03b383c8b03c6
louisb0 <louisbaragwanath@outlook.com>
2025-01-09 11:34:44 +1300

    Implement socket type

---
+++ include/internal/socket.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <condition_variable>
+#include <unordered_map>
+
+#include "common.hpp"
+
+namespace rudp::internal {
+
+struct socket {
+    linuxfd_t fd;
+    enum state_t {
+        CLOSED,
+        LISTENING,
+        SYN_SENT,
+        SYN_RCVD,
+        ESTABLISHED,
+        FIN_WAIT_1,
+        FIN_WAIT_2,
+        TIME_WAIT,
+        CLOSE_WAIT,
+        LAST_ACK,
+    } state;
+
+    std::condition_variable cv;
+
+    struct {
+    } listen_data;
+};
+
+class socket_manager {
+public:
+    [[nodiscard]] static socket_manager &instance() noexcept;
+    [[nodiscard]] rudpfd_t create() noexcept;
+    [[nodiscard]] socket *get(rudpfd_t fd) noexcept;
+
+private:
+    socket_manager() : m_next_fd(0) {}
+    ~socket_manager() = default;
+
+    socket_manager(const socket_manager &) = delete;
+    socket_manager &operator=(const socket_manager &) = delete;
+    socket_manager(socket_manager &&) = delete;
+    socket_manager &operator=(socket_manager &&) = delete;
+
+    rudpfd_t m_next_fd;
+    std::unordered_map<rudpfd_t, socket> m_sockets;
+};
+
+}  // namespace rudp::internal
---
+++ src/socket.cpp
@@ -0,0 +1,50 @@
+#include "internal/socket.hpp"
+
+#include <sys/epoll.h>
+#include <sys/socket.h>
+
+#include "internal/common.hpp"
+
+namespace rudp::internal {
+
+socket_manager &socket_manager::instance() noexcept {
+    static socket_manager manager;
+    return manager;
+}
+
+rudpfd_t socket_manager::create() noexcept {
+    linuxfd_t fd = ::socket(AF_INET, SOCK_DGRAM, 0);
+    if (fd < 0) {
+        return -1;
+    }
+
+    rudpfd_t new_fd = m_next_fd++;
+
+    struct epoll_event ev;
+    ev.events = EPOLLIN;
+    ev.data.u32 = new_fd;
+
+    // TODO: Add proper epollfd.
+    if (epoll_ctl(0, EPOLL_CTL_ADD, fd, &ev) < 0) {
+        close(fd);
+        return -1;
+    }
+
+    internal::socket socket{};
+    socket.fd = fd;
+    socket.state = socket::CLOSED;
+
+    m_sockets.emplace(new_fd, std::move(socket));
+
+    return new_fd;
+}
+
+internal::socket *socket_manager::get(rudpfd_t fd) noexcept {
+    auto it = m_sockets.find(fd);
+    if (it == m_sockets.end()) {
+        return nullptr;
+    }
+    return &it->second;
+}
+
+}  // namespace rudp::internal
