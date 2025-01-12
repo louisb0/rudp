4d6529bd65e771c22b54c56af64680317cada779
louisb0 <louisbaragwanath@outlook.com>
2025-01-12 15:03:38 +1300

    Implement bind()

--- src/rudp.cpp
+++ src/rudp.cpp
@@ -12,7 +12,42 @@ int socket(void) {
     return fd;
 }
 
-int bind(int sockfd, struct sockaddr *addr, socklen_t addrlen);
+int bind(int sockfd, struct sockaddr *addr, socklen_t addrlen) {
+    if (addr->sa_family != AF_INET) {
+        errno = EAFNOSUPPORT;
+        return -1;
+    }
+
+    auto it = internal::g_sockets.find(sockfd);
+    if (it == internal::g_sockets.end()) {
+        errno = EBADF;
+        return -1;
+    }
+
+    internal::socket *sock = &it->second;
+    if (sock->type != internal::socket::type::CREATED) {
+        errno = EOPNOTSUPP;
+        return -1;
+    }
+
+    linuxfd_t fd = ::socket(AF_INET, SOCK_DGRAM, 0);
+    if (fd < 0) {
+        return -1;
+    }
+
+    if (::bind(fd, addr, addrlen) < 0) {
+        int saved_errno = errno;
+        ::close(fd);
+        errno = saved_errno;
+        return -1;
+    }
+
+    sock->type = internal::socket::type::BOUND;
+    sock->data = fd;
+
+    return 0;
+}
+
 int listen(int sockfd, int backlog);
 int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen);
 int connect(int sockfd, struct sockaddr *addr, socklen_t addrlen);
