8dd9b1e6f60a1a90968afe4b1b675c0eca257f67
louisb0 <louisbaragwanath@outlook.com>
2025-02-21 14:53:24 +1300

    Add note on utility proxy functions

--- include/rudp.hpp
+++ include/rudp.hpp
@@ -5,6 +5,10 @@
 
 namespace rudp {
 
+// NOTE: Our interface exposes rudpfd_t as a socket handle, not the underlying file descriptor;
+// this means library users cannot call helpful utility functions such as getsockname(). It would be
+// nice to provide proxy functions for some subset of these.
+
 [[nodiscard]] int socket(void) noexcept;
 [[nodiscard]] int bind(int sockfd, struct sockaddr *addr, socklen_t addrlen) noexcept;
 [[nodiscard]] int listen(int sockfd, int backlog) noexcept;
